38df87446ded07082afef46cdd618e4467ae93ce
James Hawk <jph94880@gmail.com>
2024-02-02 13:19:22 -0600

    Moves FormContainer.jsx and SignedUpRoster.jsx from <header> to <body> in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import FormContainer from './FormContainer';
 import SignedUpRoster from './SignedUpRoster';
+import { useState, useEffect, createContext } from 'react';
+
+const API = 'http://127.0.0.1:5555/users'
+
+export const rosterContext = createContext()
 
 function App() {
+  const [roster, setRoster] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+    .then((r) => r.json())
+    .then(setRoster)
+  }, [])
+
+  console.log(roster)
+
+  // useContext() on signed up roster. 
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header></header>
+      <body className="App-body">
         <FormContainer/>
         <SignedUpRoster/>
-      </header>
+        </body>
     </div>
   );
 }
